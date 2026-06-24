64a2ccc4c1273be49a891d75346e86da2f4e0496
totSamiy73 <dmitriy73a@mail.ru>
2026-06-24 18:11:48 +0300

    Введение в Playwright. Практика с Playwright

--- playwright_registration.py
+++ playwright_registration.py
@@ -1,11 +1,14 @@
 from playwright.sync_api import sync_playwright, expect
 
 with sync_playwright() as playwright:
-    browser = playwright.firefox.launch(headless=False)
+    browser = playwright.chromium.launch(headless=False)
     page = browser.new_page()
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-    page.get_by_test_id('registration-form-email-input').locator('input').fill("user.name@gmail.com")
-    page.get_by_test_id('registration-form-username-input').locator('input').fill("username")
-    page.get_by_test_id('registration-form-password-input').locator('input').fill("password")
+    registration_email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    registration_email_input.fill("user.name@gmail.com")
+    registration_username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    registration_username_input.fill("username")
+    registration_password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    registration_password_input.fill("password")
     page.get_by_test_id('registration-page-registration-button').click()
     expect(page.get_by_test_id("dashboard-toolbar-title-text")).to_have_text("Dashboard")
