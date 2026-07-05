72f60d1a9b913b9047a6b52a82e86f31a97db66a
totSamiy73 <dmitriy73a@mail.ru>
2026-07-05 19:50:38 +0300

    Плагины Pytest

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,6 +1,12 @@
 import pytest
 from playwright.sync_api import Playwright, Page
 
+@pytest.fixture()
+def chromium_page(playwright: Playwright):
+    browser = playwright.chromium.launch(headless=False)
+    yield browser.new_page()
+    browser.close()
+
 
 @pytest.fixture(scope="session")
 def initialize_browser_state(playwright: Playwright):
